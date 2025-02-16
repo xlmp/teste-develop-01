10f6becd7b4dc805cc89019a0128cc276550c788
Leandro <ciano@ymail.com>
2025-02-16 10:51:33 -0300

    Subida de Aplicação

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Clique aqui';
+
+  Evento01(){
+    this.title = "Clicou"
+    alert("TESTE");
+  }
+}
